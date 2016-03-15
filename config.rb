114e7a78a4f51db93d01de25253f1e12addf0d84
Herman Junge <chpdg42@gmail.com>
2016-03-15 14:28:32 +0000

    Add Vagrantfile config.rb

---
+++ config.rb
@@ -0,0 +1,25 @@
+# Size of the CoreOS cluster created by Vagrant
+$num_instances=6
+
+# Used to fetch a new discovery token for a cluster of size $num_instances
+$new_discovery_url="https://discovery.etcd.io/new?size=#{$num_instances}"
+
+# Change basename of the VM
+# The default value is "core", which results in VMs named starting with
+# "core-01" through to "core-${num_instances}".
+$instance_name_prefix="core"
+
+# Change the version of CoreOS to be installed
+# To deploy a specific version, simply set $image_version accordingly.
+# For example, to deploy version 709.0.0, set $image_version="709.0.0".
+# The default value is "current", which points to the current version
+# of the selected channel
+$image_version = "current"
+
+# Official CoreOS channel from which updates should be downloaded
+$update_channel='alpha'
+
+# Customize VMs
+$vm_memory = 512
+$vm_cpus = 1
+
